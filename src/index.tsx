c01d7dd5664822ac036670005fed129cb794e4aa
Markiian Holovchak <markiianholovchak@Markiians-MacBook-Pro.local>
2024-05-05 00:39:11 +0200

    Changed basename to env public url

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <Router basename="https://markiianholovchak.github.io/meetU">
+        <Router basename={process.env.PUBLIC_URL}>
             <App />
         </Router>
     </React.StrictMode>
