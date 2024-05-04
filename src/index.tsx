793b1e0792db96c792ac0f119160688cca6d845b
Markiian Holovchak <markiianholovchak@Markiians-MacBook-Pro.local>
2024-05-05 00:35:22 +0200

    Changed router base name

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router basename="https://markiianholovchak.github.io/meetU">
             <App />
         </Router>
     </React.StrictMode>
